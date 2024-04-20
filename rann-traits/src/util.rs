f9c0e933af1fa523548c3d9110a0445d6d579e28
HeikoRibberink <heiko.ribberink@deblijdeaankomst.Null>
2024-04-20 11:37:42 +0200

    Test for function tuple struct generating utility

---
+++ rann-traits/src/util.rs
@@ -0,0 +1,14 @@
+/*!
+Utilities for common patterns in RANN.
+*/
+
+macro_rules! function_tuple {
+    (
+        $name:ident:
+        $($fs:item),+
+    ) => {
+        pub struct $name;
+
+        impl Into<
+    };
+}
